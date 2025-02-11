7d873b503b6d9e00a75f3341c0c118bff3c9b1f5
Alexis Pridemore <alpridemore@usf.edu>
2025-02-11 16:34:52 -0500

    Task 3

--- cc_example.js
+++ cc_example.js
@@ -12,16 +12,31 @@ let products = [
 
 //Task 2
 
-function applyDiscount(products, discountRate) {
-    return products.map(product => ({
-        ...product,
-        price: product.price - (product.price * discountRate)
-    }
+//function applyDiscount(products, discountRate) {
+//    return products.map(product => ({
+//        ...product,
+//        price: product.price - (product.price * discountRate)
+//    }
 
-    ))
+//    ))
     
+//};
+
+//console.log(applyDiscount(products, .1));
+
+// Task 3
+
+let sales =[250, 400, 150, 900, 1200];
+
+function calculaetTotalRevenue(sales) {
+    return sales.reduce((total, sale) =>total+sale,0)
 };
 
-console.log(applyDiscount(products, .1));
+console.log(`total Rev. 2024: $${calculateTotalRevenue(sales)}`);
 
-// 
+// Task 4
+
+let employee = {name:"John Doe", salary:50000, position: "Manager"};
+
+
+
