3712cc2216ce8c8b2cf28b7054fdcf466cd5993e
Alexis Pridemore <alpridemore@usf.edu>
2025-02-11 16:27:29 -0500

    Task 2-apply discount

--- cc_example.js
+++ cc_example.js
@@ -5,13 +5,23 @@ let products = [
     {name: "Phone", price: 800, category: "Electronics"},
 ];
 
-function getProductsByCategory(product_array, category_name) {
-    return product_array.filter(product => product.category === category);
-};
-console.log((getProductsByCategory(products, "Electronics"));
+//function getProductsByCategory(product_array, category) {
+//    return product_array.filter(product => product.category === category);
+//};
+//console.log(getProductsByCategory(products, "Electronics"));
 
 //Task 2
 
 function applyDiscount(products, discountRate) {
-    return products.map()
-}
+    return products.map(product => ({
+        ...product,
+        price: product.price - (product.price * discountRate)
+    }
+
+    ))
+    
+};
+
+console.log(applyDiscount(products, .1));
+
+// 
